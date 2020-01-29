fe953a5d954374ae31559512220ae4abbd96b3ef
Eric Moore <moor0577@algonquinlive.com>
2020-01-29 15:50:34 -0500

    adding basic testing elements

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+    console.log('Server pull active.');
 });
 
 app.listen(port, () => {

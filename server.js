4ba4ccc7780905154e4a6580371205253e6e894f
Eric Moore <moor0577@algonquinlive.com>
2020-01-29 15:41:44 -0500

    initial commit 2.0

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = require('http').createServer(app);
 
 const port = 8080;
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');

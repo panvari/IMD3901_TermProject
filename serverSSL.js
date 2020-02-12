0080d0db3d6cfd25da9a70b12ee7e642824bc9bd
Julianna Clarke <juliannaclarke@cmail.carleton.ca>
2020-02-12 18:09:15 -0500

    redirecting to mobile

--- serverSSL.js
+++ serverSSL.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + '/public'));
 
 /************* CREATE ROUTES ***************/
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + 'public/mobile.html');
 });
 
 /************* LOAD SSL CERTS (if you ran 'node createCerts.js') ***************/

71b110f5a68493e794d2c531b8beb31a3444aa4c
Eric Moore <moor0577@algonquinlive.com>
2020-02-28 12:29:20 -0500

    bug fixes

--- server.js
+++ server.js
@@ -9,43 +9,43 @@ const LISTEN_PORT = 8080;
 app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
 //routes
-app.get('/scene1', function(req,res) {
+app.get('/scene1', (req,res) => {
     res.sendFile(__dirname + '/public/scenes/01startScene.html');
 });
 
-app.get('/scene2', function(req,res) {
+app.get('/scene2', (req,res) => {
     res.sendFile(__dirname + '/public/scenes/02entrance.html');
 });
 
-app.get('/scene3', function(req,res) {
+app.get('/scene3', (req,res) => {
     res.sendFile(__dirname + '/public/scenes/03weaponRoom.html');
 });
 
-app.get('/scene4', function(req,res) {
+app.get('/scene4', (req,res) => {
     res.sendFile(__dirname + '/public/scenes/greatHall.html');
 });
 
-app.get('/scene5', function(req,res) {
+app.get('/scene5', (req,res) => {
     res.sendFile(__dirname + '/public/scenes/scene2.html');
 });
 
 
 
 //websocket stuff
-socketIO.on('connection', function(socket) {
+socketIO.on('connection', (socket) => {
     console.log(socket.id + ' has connected!');
 
-    socket.on('disconnect', function(data) {
+    socket.on('disconnect', (data) => {
         console.log(socket.id + ' has disconnected');
     });
 
     //custom events
-    socket.on('change_to_scene2', function(data) {
+    socket.on('change_to_scene2', (data) => {
         console.log('switch to entrance hall');
         socketIO.sockets.emit('scene_2');
     });
 
-    socket.on('change_to_scene1', function(data) {
+    socket.on('change_to_scene1', (data) => {
         console.log('switch to courtyard/entrance to caslte ground scene');
         socketIO.sockets.emit('scene_1');
     });

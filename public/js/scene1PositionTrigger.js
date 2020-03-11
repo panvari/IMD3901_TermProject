6fd883e9e4455a5fd03ffb86c727b82b17917392
Eric Moore <moor0577@algonquinlive.com>
2020-03-11 15:26:40 -0400

    added script for great hall position

--- public/js/scene1PositionTrigger.js
+++ public/js/scene1PositionTrigger.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent('player_position', {
 
             let playerPosition = this.el.getAttribute('position');
 
-            if(0 < playerPosition.x < 1 && playerPosition.z < -36){ //specify specific points around castle
+            if(0 < playerPosition.x < 1 && playerPosition.z < -38){ //specify specific points around castle
                 // console.log('change to scene 2 (Great Hall) ')
                 changeScene(2); //call changescene function within the startScene imbedded html script
             };

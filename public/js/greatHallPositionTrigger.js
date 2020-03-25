e2a07685008298c28222ac25b9d40dee21fb431d
ericmoore123 <51104403+ericmoore123@users.noreply.github.com>
2020-03-25 19:21:23 -0400

    physics implementations

--- public/js/greatHallPositionTrigger.js
+++ public/js/greatHallPositionTrigger.js
@@ -9,12 +9,12 @@ AFRAME.registerComponent('player_position', {
 
         let playerPosition = this.el.getAttribute('position');
 
-        if (0 < playerPosition.x < 1 && playerPosition.z < -39.5) { //specify specific points around castle
+        if (0 < playerPosition.x < 1 && playerPosition.z < -31) { //specify specific points around castle
             // console.log('change to scene 2 (Great Hall) ')
             changeScene(3); //call changescene function within the startScene imbedded html script
         }
 
-        if (0 < playerPosition.x < 1 && 3.5 < playerPosition.z) { //specify specific points around castle
+        if (0 < playerPosition.x < 1 && playerPosition.z > 3) { //specify specific points around castle
             // console.log('change to scene 2 (Great Hall) ')
             changeScene(1); //call changescene function within the startScene imbedded html script
         }
--- public/js/throneRoomPositionTrigger.js
+++ public/js/throneRoomPositionTrigger.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent('player_position', {
 
         let playerPosition = this.el.getAttribute('position');
 
-        if (-0.5 < playerPosition.x < 1.5 && playerPosition.z > 9.5) { //specify specific points around castle
+        if (-1 < playerPosition.x < 1 && playerPosition.z > 9.5) { //specify specific points around castle
             // console.log('change to scene 2 (Great Hall) ')
             changeScene(3.1); //call changescene
         };

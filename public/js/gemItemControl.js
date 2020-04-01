51651040b65b7e3ff5115a8fee812308cbbb5b27
ericmoore123 <51104403+ericmoore123@users.noreply.github.com>
2020-03-31 21:01:09 -0400

    Added score system and gems to desktop pages

---
+++ public/js/gemItemControl.js
@@ -0,0 +1,155 @@
+AFRAME.registerComponent('weaponsgem', {
+    schema: {
+        // Def Values
+    },
+
+    init: function() { //DONE
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let armourInstructions = document.querySelector('#armourInstructions');
+        let weaponRoomGem = document.querySelector('#weaponRoomGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                weaponRoomGem.setAttribute('material', 'color', 'red')
+                armourInstructions.setAttribute('visible', 'true');
+                window.sessionStorage.setItem('gem2Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_mini_game1') //Call Minigame 1 for mobile
+            } else if (counter % 2 == 1) {
+                armourInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
+
+AFRAME.registerComponent('traininggem', {
+    schema: {
+        // Def Values
+    },
+
+    init: function() {
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let trainingInstructions = document.querySelector('#trainingInstructions');
+        let trainingGem = document.querySelector('#trainingGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                trainingInstructions.setAttribute('visible', 'true');
+                trainingGem.setAttribute('material', 'color', 'red');
+                window.sessionStorage.setItem('gem5Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_mini_game1') //Call Minigame 1 for mobile
+            } else if (counter % 2 == 1) {
+                trainingInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
+
+AFRAME.registerComponent('servantsgem', { //DONE
+    schema: {
+        // Def Values
+    },
+
+    init: function() {
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let servantsInstructions = document.querySelector('#servantInstructions');
+        let servantsGem = document.querySelector('#servantsGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                servantsInstructions.setAttribute('visible', 'true');
+                servantsGem.setAttribute('material', 'color', 'red')
+                window.sessionStorage.setItem('gem1Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_mini_game1') //Call Minigame 1 for mobile
+            } else if (counter % 2 == 1) {
+                servantsInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
+
+AFRAME.registerComponent('pantrygem', { // DONE
+    schema: {
+        // Def Values
+    },
+
+    init: function() {
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let pantryInstructions = document.querySelector('#pantryInstructions');
+        let pantryGem = document.querySelector('#pantryGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                pantryInstructions.setAttribute('visible', 'true');
+                pantryGem.setAttribute('material', 'color', 'red');
+                window.sessionStorage.setItem('gem3Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_mini_game1') //Call Minigame 1 for mobile
+            } else if (counter % 2 == 1) {
+                pantryInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
+
+AFRAME.registerComponent('towergem', { //DONE
+    schema: {
+        // Def Values
+    },
+
+    init: function() {
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let towerInstructions = document.querySelector('#towerInstructions');
+        let towerGem = document.querySelector('#towerGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                towerInstructions.setAttribute('visible', 'true');
+                towerGem.setAttribute('material', 'color', 'red');
+                window.sessionStorage.setItem('gem4Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_tower_MiniGame') //Call Minigame 1 for mobile
+            } else if (counter % 2 == 1) {
+                towerInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
+
+AFRAME.registerComponent('thronegem', {
+    schema: {
+        // Def Values
+    },
+
+    init: function() { //DONE
+        let counter = 0;
+        const gemNoise = new Audio("/audio/gemNoise.wav");
+        let kingColorInstructions = document.querySelector('#kingColorInstructions');
+        let throneRoomGem = document.querySelector('#throneRoomGem')
+
+        this.el.addEventListener('click', () => {
+            if (counter % 2 == 0) {
+                kingColorInstructions.setAttribute('visible', 'true');
+                throneRoomGem.setAttribute('material', 'color', 'red');
+                window.sessionStorage.setItem('gem6Collected', 'true');
+                gemNoise.play();
+                socket.emit('launch_Minigame') //Call Minigame for mobile
+            } else if (counter % 2 == 1) {
+                kingColorInstructions.setAttribute('visible', 'false');
+            }
+            counter += 1;
+        });
+    }
+});
--- public/js/scoreSystem.js
+++ public/js/scoreSystem.js
@@ -1,42 +1,34 @@
-    //UI Gems
+    //UI Gem Elements
     gem1 = document.querySelector('#gem1')
     gem2 = document.querySelector('#gem2')
-        // gem3 = document.querySelector('#gem3')
-        // gem4 = document.querySelector('#gem4')
-        // gem5 = document.querySelector('#gem5')
-        // gem6 = document.querySelector('#gem6')
-
-    //Update click bool variables
-    gemItem1 = document.querySelector("#gemItem1");
-    gemItem1.addEventListener('click', () => { //first gem event listener
-        console.log('clicked gem1')
-        gem1.setAttribute('opacity', '1');
-        gemItem1.setAttribute('material', 'color', 'red');
-
-        //store collection value between pages
-        window.sessionStorage.setItem('gem1Collected', 'true');
-    });
+    gem3 = document.querySelector('#gem3')
+    gem4 = document.querySelector('#gem4')
+    gem5 = document.querySelector('#gem5')
+    gem6 = document.querySelector('#gem6')
 
     setInterval((data) => {
-        if (sessionStorage.getItem('gem1Collected') === 'true') {
+        if (sessionStorage.getItem('gem1Collected') == 'true') {
             gem1.setAttribute('opacity', '1');
-            gemItem1.setAttribute('material', 'color', 'red');
+        }
 
-        }
-        if (sessionStorage.getItem('gem2Collected') === 'true') {
+        if (sessionStorage.getItem('gem2Collected') == 'true') {
             gem2.setAttribute('opacity', '1');
-            gemItem2.setAttribute('visible', 'false');
         }
-        // if(localStorage.getItem('gem4Collected') === 'true'){
-        //     gem4.setAttribute('opacity','1');
-        //     gemItem.setAttribute('visible', 'false');
-        // }
-        // if(localStorage.getItem('gem5Collected') === 'true'){
-        //     gem5.setAttribute('opacity','1');
-        //     gemItem.setAttribute('visible', 'false');
-        // }
-        // if(localStorage.getItem('gem6Collected') === 'true'){
-        //     gem6.setAttribute('opacity','1');
-        //     gemItem.setAttribute('visible', 'false');
-        // }
+
+        if (sessionStorage.getItem('gem3Collected') == 'true') {
+            console.log('setting gem color on ui')
+            gem3.setAttribute('opacity', '1');
+        }
+
+        if (sessionStorage.getItem('gem4Collected') == 'true') {
+            gem4.setAttribute('opacity', '1');
+        }
+
+        if (sessionStorage.getItem('gem5Collected') == 'true') {
+            gem5.setAttribute('opacity', '1');
+        }
+
+        if (sessionStorage.getItem('gem6Collected') == 'true') {
+            gem6.setAttribute('opacity', '1');
+        }
     }, 1000);
